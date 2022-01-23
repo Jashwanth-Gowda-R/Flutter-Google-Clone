f13b20baa4f1635e8f028a33293fd7a8402029d9
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-01-23 15:45:08 +0530

    added web screen appbar

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -1,11 +1,68 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:google_clone/colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Gmail',
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Images',
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          IconButton(
+              icon: SvgPicture.asset(
+                'images/more-apps.svg',
+                color: primaryColor,
+              ),
+              onPressed: () {}),
+          SizedBox(
+            width: 10,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 10).copyWith(right: 10),
+            child: MaterialButton(
+              onPressed: () {},
+              color: Color(0xff1A73EB),
+              child: Text(
+                'Sign in',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
       body: Center(child: Text('hello from web')),
     );
   }
