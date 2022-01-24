8c958b6e8fd69834f725af1465127e1244321917
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-01-25 00:01:05 +0530

    added search tabs in search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,10 +1,28 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:google_clone/widgets/search_header.dart';
+import 'package:google_clone/widgets/search_tabs.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SearchHeader(),
+            Padding(
+              padding: const EdgeInsets.only(left: 150.0),
+              child: SearchTabs(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
