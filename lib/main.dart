8b1cf3aac6a44368a5489e1fe1038fa5ad28346d
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-03-09 18:15:25 +0530

    changed app name

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Shani Google',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
