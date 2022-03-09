e90ae8f48974d74d505a59ea8af9ac581ea451fd
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-03-09 17:53:20 +0530

    changed search screen page tab title

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -18,131 +18,137 @@ class SearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SearchHeader(),
-            Padding(
-              padding: EdgeInsets.only(left: size.width <= 768 ? 10 : 150.0),
-              child: SearchTabs(),
-            ),
-            Divider(
-              height: 0,
-              thickness: 0.3,
-            ),
-            FutureBuilder(
-              future: ApiService().fetchData(
-                context: context,
-                queryTerm: searchQuery,
-                start: start,
+    return Title(
+      color: Colors.blue,
+      title: searchQuery,
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SearchHeader(),
+              Padding(
+                padding: EdgeInsets.only(left: size.width <= 768 ? 10 : 150.0),
+                child: SearchTabs(),
               ),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.hasData) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.only(
-                          left: size.width <= 768 ? 10 : 150,
-                          top: 12,
-                        ),
-                        child: Text(
-                          'About ${snapshot.data?['searchInformation']['formattedTotalResults']} results  (${snapshot.data?['searchInformation']['formattedSearchTime']} seconds)',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.grey[500],
+              Divider(
+                height: 0,
+                thickness: 0.3,
+              ),
+              FutureBuilder(
+                future: ApiService().fetchData(
+                  context: context,
+                  queryTerm: searchQuery,
+                  start: start,
+                ),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(
+                            left: size.width <= 768 ? 10 : 150,
+                            top: 12,
+                          ),
+                          child: Text(
+                            'About ${snapshot.data?['searchInformation']['formattedTotalResults']} results  (${snapshot.data?['searchInformation']['formattedSearchTime']} seconds)',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.grey[500],
+                            ),
                           ),
                         ),
-                      ),
-                      ListView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: snapshot.data?['items'].length,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: EdgeInsets.only(
-                                left: size.width <= 768 ? 10 : 150, top: 10),
-                            child: SearchResultComponent(
-                              linkToGo: snapshot.data?['items'][index]['link'],
-                              link: snapshot.data?['items'][index]
-                                  ['formattedUrl'],
-                              text: snapshot.data?['items'][index]['title'],
-                              desc: snapshot.data?['items'][index]['snippet'],
-                            ),
-                          );
-                        },
-                      ),
-                      // pagination btns
-                      Container(
-                        width: double.infinity,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            TextButton(
-                              onPressed: start != "0"
-                                  ? () {
-                                      Navigator.of(context).push(
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  SearchScreen(
-                                                    searchQuery: searchQuery,
-                                                    start:
-                                                        (int.parse(start) - 10)
-                                                            .toString(),
-                                                  )));
-                                    }
-                                  : () {},
-                              child: Text(
-                                '< Prev',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  color: blueColor,
-                                ),
+                        ListView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: snapshot.data?['items'].length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: EdgeInsets.only(
+                                  left: size.width <= 768 ? 10 : 150, top: 10),
+                              child: SearchResultComponent(
+                                linkToGo: snapshot.data?['items'][index]
+                                    ['link'],
+                                link: snapshot.data?['items'][index]
+                                    ['formattedUrl'],
+                                text: snapshot.data?['items'][index]['title'],
+                                desc: snapshot.data?['items'][index]['snippet'],
                               ),
-                            ),
-                            SizedBox(
-                              width: 30,
-                            ),
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (context) => SearchScreen(
-                                      searchQuery: searchQuery,
-                                      start: (int.parse(start) + 10).toString(),
-                                    ),
-                                  ),
-                                );
-                              },
-                              child: Text(
-                                'Next >',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  color: blueColor,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 30,
-                            ),
-                          ],
+                            );
+                          },
                         ),
-                      ),
-                      SearchFooter(),
-                    ],
+                        // pagination btns
+                        Container(
+                          width: double.infinity,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              TextButton(
+                                onPressed: start != "0"
+                                    ? () {
+                                        Navigator.of(context).push(
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    SearchScreen(
+                                                      searchQuery: searchQuery,
+                                                      start: (int.parse(start) -
+                                                              10)
+                                                          .toString(),
+                                                    )));
+                                      }
+                                    : () {},
+                                child: Text(
+                                  '< Prev',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: blueColor,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 30,
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) => SearchScreen(
+                                        searchQuery: searchQuery,
+                                        start:
+                                            (int.parse(start) + 10).toString(),
+                                      ),
+                                    ),
+                                  );
+                                },
+                                child: Text(
+                                  'Next >',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: blueColor,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 30,
+                              ),
+                            ],
+                          ),
+                        ),
+                        SearchFooter(),
+                      ],
+                    );
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(),
                   );
-                }
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
